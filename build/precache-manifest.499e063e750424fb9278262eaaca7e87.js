6ba5cc7079a94d77e6d8feb34e1f431497ecd26e
Jacqueline Dean <jacquelineadean@gmail.com>
2020-08-12 15:52:35 -0500

    Initialized React App and added favicon logo

---
+++ build/precache-manifest.499e063e750424fb9278262eaaca7e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7e0ab2f5283929db0d549c7e9ec8bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77058fad6a7595d64e73",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb9511e447f7bfb108b7",
+    "url": "/static/js/2.73518ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73518ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77058fad6a7595d64e73",
+    "url": "/static/js/main.e3ff493c.chunk.js"
+  },
+  {
+    "revision": "c22f6608ac57bf3e670b",
+    "url": "/static/js/runtime-main.1cdbb726.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
